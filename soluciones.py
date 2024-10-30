7d65c80d5b99923a8793f6c35d96c3eaf68b1c5b
PaulaAndreaPobladoGarcia <182712699+PaulaAndreaPobladoGarcia@users.noreply.github.com>
2024-10-30 13:19:47 -0500

    solucion_hora

--- soluciones.py
+++ soluciones.py
@@ -1,4 +1,4 @@
-firstcatetus = float(input("What's the first leg of a triangle?: "))
-secondcatetus = float(input("What's the second leg of a triangle?: "))
-hypotenuse = ((firstcatetus ** 2 + secondcatetus ** 2) ** 0.5)
-print(f"""The hypotenuse is: {hypotenuse}""")
+current_time = int(input("Enter the current hour (0-23): "))
+number_hours = int(input("Number of hours: "))
+new_time = (current_time + number_hours) %24
+print (f"In {number_hours} hours, the clock will read {new_time}") 
