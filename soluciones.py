d4a9144dd1a0418994c57a4e7844eaf97c61dd9d
PaulaAndreaPobladoGarcia <182712699+PaulaAndreaPobladoGarcia@users.noreply.github.com>
2024-10-30 13:19:12 -0500

    solucion_pitagoras

--- soluciones.py
+++ soluciones.py
@@ -1,3 +1,4 @@
-number = input("Enter a three-digit integer: ")
-reversedNumber = number[::-1]
-print(f"The reversed number is: {reversedNumber}")
+firstcatetus = float(input("What's the first leg of a triangle?: "))
+secondcatetus = float(input("What's the second leg of a triangle?: "))
+hypotenuse = ((firstcatetus ** 2 + secondcatetus ** 2) ** 0.5)
+print(f"""The hypotenuse is: {hypotenuse}""")
