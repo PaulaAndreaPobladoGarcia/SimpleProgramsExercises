99d627e9f8dd7652a5efde399452d6c8dc87c42f
PaulaAndreaPobladoGarcia <182712699+PaulaAndreaPobladoGarcia@users.noreply.github.com>
2024-10-30 13:16:36 -0500

    solucion_Conversión_de_unidades

--- soluciones.py
+++ soluciones.py
@@ -1,6 +1,3 @@
-first_grade = float(input("First grade: "))
-second_grade = float(input("Second grade: "))
-third_grade = float(input("Third grade: "))
-fourth_grade = float(input("Fourth grade: "))
-average = (first_grade + second_grade + third_grade + fourth_grade) / 4
-print(f"The average is: {average:.2f}")
+centimeters = float(input("Enter the measurement in centimeters to convert to inches: "))
+inches = centimeters / 2.54
+print(f"The measurement in inches is: {inches}")
