5898b284f20689d8298a971cf4e086d7f9fbb5e7
PaulaAndreaPobladoGarcia <182712699+PaulaAndreaPobladoGarcia@users.noreply.github.com>
2024-10-30 13:24:12 -0500

    solucion_que_nota_necesito

--- soluciones.py
+++ soluciones.py
@@ -1,3 +1,7 @@
-real_number = float(input("Enter a real number: "))
-decimal_part = real_number - int(real_number)
-print(f"The decimal part of {real_number} is: {decimal_part}")
+C1 = int(input("Enter grade for assessment 1: "))
+C2 = int(input("Enter grade for assessment 2: "))
+NL = int(input("Enter lab grade: "))
+
+NC = (59.5 - (0.3 * NL)) / 0.7
+C3 = (((3 * NC) - (C1 + C2)) + 1)
+print(round(C3))
